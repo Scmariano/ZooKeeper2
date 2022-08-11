fe62a2e444b805d534b27ea24efc7219cc015e53
Scmariano <104543069+Scmariano@users.noreply.github.com>
2022-08-11 13:06:16 -0700

    commit

--- src/com/codingdojo/zookeeper2/Bat.java
+++ src/com/codingdojo/zookeeper2/Bat.java
@@ -5,15 +5,18 @@ public class Bat extends Mammal {
 	public void fly() {
 		System.out.println("HHHkkkkk!");
 		this.setEnergyLevel(-50);
+		displayEnergy();
 	}
 	
 	public void eatHumans() {
 		System.out.println("so -- well, never mind!");
 		this.setEnergyLevel(25);
+		displayEnergy();
 	}
 	
 	public void attackTown() {
 		System.out.println("The town is on fire!");
 		this.setEnergyLevel(-100);
+		displayEnergy();
 	}
 }
